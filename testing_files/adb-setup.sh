dfe6a55f5bba89e3b6b000dced62fed4e68f6013
Dav Clark <davclark@berkeley.edu>
2015-08-07 20:37:14 +0900

    full setup for a blank tablet

--- testing_files/adb-setup.sh
+++ testing_files/adb-setup.sh
@@ -1,10 +1,12 @@
 adb install -r ../collect_app/build/outputs/apk/collect_app-debug.apk
-adb install Lumosity.apk # OK if this fails
+adb install Lumosity.apk # OK if this fails, but it's slow
 # This is suggested approach for ODK Briefcase... hopefully this will work
 # fine! https://opendatakit.org/use/briefcase/
 adb push NatsuZemi.xml /sdcard/odk/forms
 # Not sure we need this
 adb shell mkdir /sdcard/odk/forms/NatsuZemi-media
-adb shell rm "/sdcard/odk/forms/Let s Start.xml"
-adb shell rmdir "/sdcard/odk/forms/Let s Start-media"
+# Sadly, there's still some additional entry somewhere that results in a broken
+# form. Currently doing via manual interface.
+# adb shell rm "/sdcard/odk/forms/Let s Start.xml"
+# adb shell rmdir "/sdcard/odk/forms/Let s Start-media"
 
