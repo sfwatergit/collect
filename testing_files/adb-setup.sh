2adf0dee656b93c103d5a85040e614c87db68d9f
Dav Clark <davclark@berkeley.edu>
2015-08-07 14:23:22 +0900

    Minodr updates, close to final!

--- testing_files/adb-setup.sh
+++ testing_files/adb-setup.sh
@@ -2,5 +2,9 @@ adb install -r ../collect_app/build/outputs/apk/collect_app-debug.apk
 adb install Lumosity.apk # OK if this fails
 # This is suggested approach for ODK Briefcase... hopefully this will work
 # fine! https://opendatakit.org/use/briefcase/
-adb push NatsuZemi-media /sdcard/odk/forms
 adb push NatsuZemi.xml /sdcard/odk/forms
+# Not sure we need this
+adb shell mkdir /sdcard/odk/forms/NatsuZemi-media
+adb shell rm "/sdcard/odk/forms/Let s Start.xml"
+adb shell rmdir "/sdcard/odk/forms/Let s Start-media"
+
